df2630d948a2f576da810e5a60c535143f31c6be
Jorge Matos <jisapjisap@gmail.com>
2025-11-01 19:40:21 +0100

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,28 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Chakra_Petch,  Mona_Sans, Poppins, Unbounded } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const chakrapetch = Chakra_Petch({
+  variable: "--font-chakrapetch",
+  weight: ["300", "400", "500", "600", "700"],
+  subsets: ["latin"]
+});
+
+const mona = Mona_Sans({
+  variable: "--font-mona",
+  weight: ["300", "400", "500", "600"],
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppins = Poppins ({
+  variable: "--font-poppins",
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
+
+const unbounded = Unbounded({
+  variable: "--font-unbounded",
+  weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
@@ -25,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${chakrapetch.variable} ${mona.variable} ${poppins.variable} ${unbounded.variable} antialiased`}
       >
         {children}
       </body>
