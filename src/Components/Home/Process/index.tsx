82e102d682a204f31cfb6e252e5099576831ba3d
Jorge Matos <jisapjisap@gmail.com>
2025-11-07 19:31:16 +0100

    Add Process component to Home page; implement layout and styling for process section with call-to-action link and Swiper integration

---
+++ src/Components/Home/Process/index.tsx
@@ -0,0 +1,60 @@
+"use client"
+
+import React, { FC } from 'react'
+import { solutionData } from "@/app/api/data"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay, Navigation } from "swiper/modules"
+import "swiper/css"
+import "swiper/css/navigation"
+import "swiper/css/pagination"
+import "swiper/css/autoplay"
+import { Icon } from '@iconify/react'
+import Link from 'next/link'
+
+
+
+
+const Process: FC = () => {
+  return (
+    <>
+      <section className='bg-light overflow-hidden py-14 lg:py-18 xl:py-22 bg-prim-light'>
+        <div className="container mx-auto lg:max-w-[--breakpoint-xl] md:max-w-[--breakpoint-md] px-4 space-y-14">
+          <div className='solution-content flex justify-between items-start lg:items-end gap-2 flex-col lg:flex-row'>
+            <div>
+              <span className='sub-title text-14 bg-prim text-white py-1 rounded-xl relative font-chakrapetch capitalize ps-5 pe-3'>
+                Our Process
+              </span>
+
+              <h2 className='w-full lg:w-3/4 mt-4 font-chakrapetch lg:text-35 font-semibold'>
+                Seamless Process, Great Results.
+              </h2>
+            </div>
+
+            <div className='space-x-3 flex xl:flex-row flex-col xl:space-y-0 space-y-3'>
+              <p className='text-pera-dark text-16 pb-0 lg:pb-0 xl:pb-3'>
+                Developing personalized customer journeys to increase satisfaction and loyalty.
+              </p>
+            </div>
+
+            <Link
+              href="/constact"
+              className='text-white bg-dark h-[50px] lg:text-16 text-sm xl:w-[230px] w-fit rounded-full font-chakrapetch font-semibold flex gap-2 ps-4 pe-2 py-2 justify-center items-center tracking-wider group mt-5 lg:mt-0'
+            >
+              Request a Call
+              <Icon
+                icon="tabler:arrow-right"
+                width="24"
+                height="24"
+                className='bg-prim text-white rounded-full h-full w-[35px] p-1.5 group-hover:-rotate-45 tansition-all duration-300'
+              />
+            </Link>
+          </div>
+        </div>
+
+
+      </section>
+    </>
+  )
+}
+
+export default Process
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Commitment from "@/Components/Home/Commitment";
 import Companies from "@/Components/Home/Companies";
 import Hero from "@/Components/Home/Hero";
+import Process from "@/Components/Home/Process";
 import Solution from "@/Components/Home/Solution";
 import Image from "next/image";
 
@@ -12,6 +13,7 @@ export default function Home() {
         <Solution />
         <Companies />
         <Commitment />
+        <Process />
       </main>
     </>
   );
