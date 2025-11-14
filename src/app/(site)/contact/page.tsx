7a6ee036bd448dc41533e201ecba22c55ca9ec06
Jorge Matos <jisapjisap@gmail.com>
2025-11-14 18:30:21 +0100

    Adjust contact page layout by modifying the height of the Google Maps iframe for better visual consistency; ensure responsive design remains intact across different screen sizes.

--- src/app/(site)/contact/page.tsx
+++ src/app/(site)/contact/page.tsx
@@ -115,7 +115,7 @@ const page = () => {
               </form>
             </div>
 
-            <div className='w-full lg:w-[50%] lg:h-[500px] mt-10'>
+            <div className='w-full lg:w-[50%] h-[300px] lg:h-[500px] mt-10'>
               <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d194347.47827017203!2d-3.844343387490552!3d40.43809861028444!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd422997800a3c81%3A0xc436dec1618c2269!2sMadrid!5e0!3m2!1ses!2ses!4v1763140830874!5m2!1ses!2ses" width="100%" height="100%"  loading="lazy"></iframe>
             </div>
           </div>
