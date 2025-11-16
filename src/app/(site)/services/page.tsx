9ce40c5a7312f7593c9bfaa7afdfa455cf03e2b2
Jorge Matos <jisapjisap@gmail.com>
2025-11-16 22:32:09 +0100

    Refactor ServicesPage layout to improve content structure and user interaction; replace Link with div for better styling control, enhance service details presentation, and add a Learn More button for each service.

--- src/app/(site)/services/page.tsx
+++ src/app/(site)/services/page.tsx
@@ -41,12 +41,11 @@ const ServicesPage = () => {
                 service.slug ?? createSlug(service.shortTitle ?? '');
 
               return (
-                <Link
+                <div
                   key={service.id}
-                  href={`/services/${slug}`}
-                  className="group block bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-lg transition-all duration-300"
+                  className="group bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-lg transition-all duration-300 flex flex-col"
                 >
-                  <div className="relative w-full h-60">
+                  <div className="relative w-full h-60 overflow-hidden">
                     <Image
                       src={service.img}
                       alt={service.title}
@@ -54,15 +53,25 @@ const ServicesPage = () => {
                       className="object-cover group-hover:scale-105 transition-transform duration-300"
                     />
                   </div>
-                  <div className="p-6">
-                    <h3 className="text-xl font-semibold mb-2 text-gray-900 group-hover:text-prim transition-colors">
-                      {service.shortTitle}
-                    </h3>
-                    <p className="text-gray-600 text-sm leading-relaxed">
-                      {service.shortDesc}
-                    </p>
+
+                  <div className="p-6 flex flex-col grow 2xl:flex-row 2xl:justify-between 2xl:items-end">
+                    <div className="grow mb-4 2xl:mb-0">
+                      <Link href={`/services/${slug}`}>
+                        <h3 className="text-xl font-semibold mb-2 text-gray-900 hover:text-prim transition-colors">
+                          {service.shortTitle}
+                        </h3>
+                      </Link>
+                      
+                      <p className="text-gray-600 text-sm leading-relaxed">
+                        {service.shortDesc}
+                      </p>
+                    </div>
+
+                    <div className="mt-auto 2xl:mt-0 pt-4 2xl:pt-0 2xl:ps-4 shrink-0">
+                      <ActionButton href={`/services/${slug}`} text="Learn More" variant='light' />
+                    </div>
                   </div>
-                </Link>
+                </div>
               );
             })}
           </div>
