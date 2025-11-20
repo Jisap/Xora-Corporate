d1ea116b9ed4aa805a4a6e08c7c1321b47697b99
Jorge Matos <jisapjisap@gmail.com>
2025-11-20 16:09:07 +0100

    feat: Introduce Solution component with an animated Swiper carousel and add Framer Motion variants for Home sections.

--- src/Components/Home/Hero/animation.ts
+++ src/Components/Home/Hero/animation.ts
@@ -24,3 +24,18 @@ export const fadeInFromRightWithDelay: Variants = {
     }
   },
 };
+
+export const containerVariants: Variants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2, // Retraso entre la animación de cada tarjeta
+    },
+  },
+};
+
+export const itemVariants: Variants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5, ease: "easeOut" } },
+};
--- src/Components/Home/Solution/index.tsx
+++ src/Components/Home/Solution/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { FC } from 'react'
+import React, { FC, useRef } from 'react'
 import { solutionData } from "@/app/api/data"
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay, Navigation } from "swiper/modules"
@@ -11,25 +11,44 @@ import "swiper/css/autoplay"
 import SectionHeader from '../../SharedComponents/SectionHeader'
 import ActionButton from '../../SharedComponents/ActionButton'
 import SolutionCard from './SolutionCard'
+import { motion, useInView } from 'framer-motion'
+import { containerVariants, itemVariants } from './animation';
 
 
 const Solution: FC = () => {
+  const headerRef = useRef(null);
+  const swiperRef = useRef(null);
+  const isHeaderInView = useInView(headerRef, { once: true, amount: 0.5 });
+  const isSwiperInView = useInView(swiperRef, { once: true, amount: 0.2 });
+
   return (
     <>
       <section className='bg-light overflow-hidden py-14 lg:py-18 xl:py-22 bg-prim-light'>
         <div className="container mx-auto lg:max-w-[--breakpoint-xl] md:max-w-[--breakpoint-md] px-4 space-y-14">
-          <div className="flex justify-between items-start lg:items-end gap-2 flex-col lg:flex-row">
-            <SectionHeader 
+          <motion.div
+            ref={headerRef}
+            initial={{ opacity: 0, y: 50 }}
+            animate={isHeaderInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+            className="flex justify-between items-start lg:items-end gap-2 flex-col lg:flex-row"
+          >
+            <SectionHeader
               subtitle="Our Solution"
-              title="Tailor-Made Bussiness Solutions for Modern Corporates"
+              title="Tailor-Made Business Solutions for Modern Corporates"
             />
             <ActionButton
               href="/services"
               text="Explore More"
             />
-          </div>
+          </motion.div>
 
-          <div className='relative mt-10'>
+          <motion.div
+            ref={swiperRef}
+            className='relative mt-10'
+            variants={containerVariants}
+            initial="hidden"
+            animate={isSwiperInView ? "visible" : "hidden"}
+          >
             <Swiper
               modules={[Autoplay, Navigation]}
               spaceBetween={20}
@@ -48,14 +67,16 @@ const Solution: FC = () => {
                 1280: { slidesPerView: 4 },
               }}
               className='solution-wrapper'
-            > 
+            >
               {solutionData.map((solution, index) => (
-                <SwiperSlide key={index}> 
-                  <SolutionCard solution={{ ...solution, link: '/services' }} />
+                <SwiperSlide key={index} >
+                  <motion.div variants={itemVariants}>
+                    <SolutionCard solution={{ ...solution, link: '/services' }} />
+                  </motion.div>
                 </SwiperSlide>
               ))}
             </Swiper>
-          </div>
+          </motion.div>
         </div>
       </section>
     </>
